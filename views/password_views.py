27d723af2e8407246325a6a2586185d3cc1bc307
oiviana <vianalucas80@gmail.com>
2024-11-07 21:20:53 -0300

    feat: adding random string const

--- views/password_views.py
+++ views/password_views.py
@@ -1,8 +1,11 @@
+import string
+
 class FernetHasher:
+    RANDOM_STRING_CHARS = string.ascii_lowercase + string.ascii_uppercase
 
     @classmethod
     def _get_random_string(cls):
-        print('Hello World')
+        print()
 
 
 FernetHasher._get_random_string()
