7bda184fa89d807021caca067343319afc275659
oiviana <vianalucas80@gmail.com>
2024-11-07 21:27:20 -0300

    feat: creating a random string

--- views/password_views.py
+++ views/password_views.py
@@ -1,11 +1,16 @@
-import string
+import string, secrets
 
 class FernetHasher:
     RANDOM_STRING_CHARS = string.ascii_lowercase + string.ascii_uppercase
 
     @classmethod
     def _get_random_string(cls):
-        print()
+        string = ''
+        for i in range(25):
+           string += secrets.choice(cls.RANDOM_STRING_CHARS)
+           print(string)
+           
+        return string
 
 
 FernetHasher._get_random_string()
