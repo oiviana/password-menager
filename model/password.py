b691619c42d555c44e34b2ed758b9482860371d6
oiviana <vianalucas80@gmail.com>
2024-11-08 22:49:49 -0300

    test: writting class content

--- model/password.py
+++ model/password.py
@@ -2,14 +2,20 @@ from datetime import datetime
 from pathlib import Path
 
 class BaseModel:
+    # criando o diretório da base de dados
     BASE_DIR = Path(__file__).resolve().parent.parent 
     DB_DIR = BASE_DIR / 'db'
 
     def save(self):
         table_path = Path(self.DB_DIR / f'{self.__class__.__name__}.txt')
-
+        
+        # se não houver o arquivo, ele será criado
         if not table_path.exists():
             table_path.touch()
+
+        with open(table_path, 'a') as arq:
+            arq.write("|".join(list(map(str, self.__dict__.values()))))
+            arq.write('\n')
 
 
 class Password(BaseModel):
@@ -19,5 +25,5 @@ class Password(BaseModel):
         self.created_at = datetime.now().isoformat()
 
 
-p1 = Password(domain='111', password='2323')
+p1 = Password(domain='Facenugget', password='2323')
 p1.save()
