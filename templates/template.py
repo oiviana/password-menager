7e90b35839ee81b8ff55e59e8a2fd2975c693cc7
oiviana <vianalucas80@gmail.com>
2024-11-09 14:25:53 -0300

    feat: adding decrypt option

--- templates/template.py
+++ templates/template.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.curdir))
 from model.password import Password
 from views.password_views import FernetHasher
 
-action = input('Digite 1 para salvar uma nova senha ou 2 para visualizar uma senha salva')
+action = input('Digite 1 para salvar uma nova senha ou 2 para visualizar uma senha salva: ')
 
 match action:
     case '1':
@@ -25,6 +25,19 @@ match action:
         fernet_user = FernetHasher(key)
         p1 = Password(domain=domain, password=fernet_user.encrypt(password).decode('utf-8'))
         p1.save()   
-        
+
     case '2':
-        ...
+        domain = input('Domínio: ')
+        key = input('Key: ')
+        fernet_user = FernetHasher(key)
+        data = Password.get()
+
+        for i in data:
+            if domain in i['domain']:
+                password = fernet_user.decrypt(i['password'])
+                
+        if password:
+            print(f'Sua senha: {password}')
+
+        else:
+            print('Nenhuma senha encontrada para esse domínio :(')
