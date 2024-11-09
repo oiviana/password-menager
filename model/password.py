213b5de3b505221563fbd5b10810fdf54cdcd8d5
oiviana <vianalucas80@gmail.com>
2024-11-09 13:53:30 -0300

    feat: adding read file function

--- model/password.py
+++ model/password.py
@@ -16,14 +16,31 @@ class BaseModel:
         with open(table_path, 'a') as arq:
             arq.write("|".join(list(map(str, self.__dict__.values()))))
             arq.write('\n')
+    
+    # lendo o conteúdo do arquivo
+    @classmethod
+    def get(cls):
+        table_path = Path(cls.DB_DIR / f'{cls.__name__}.txt')
+        
+        if not table_path.exists():
+            table_path.touch()
 
+        with open(table_path, 'r') as arq:
+            x = arq.readlines()
+
+        results = []
+
+        attr = vars(cls())
+
+        for i in x:
+            split_v = i.split('|')
+            tmp_dict = dict(zip(attr, split_v))
+            results.append(tmp_dict)
+
+        return results
 
 class Password(BaseModel):
     def __init__(self, domain=None, password=None, expire=False):
         self.domain = domain
         self.password = password
         self.created_at = datetime.now().isoformat()
-
-
-p1 = Password(domain='Facenugget', password='2323')
-p1.save()
