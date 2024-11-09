aa28e541c1a7a6850cefca62ced1fc99e6bfcb0b
oiviana <vianalucas80@gmail.com>
2024-11-09 14:16:15 -0300

    feat: creating input template

---
+++ templates/template.py
@@ -0,0 +1,30 @@
+import sys, os
+
+sys.path.append(os.path.abspath(os.curdir))
+
+from model.password import Password
+from views.password_views import FernetHasher
+
+action = input('Digite 1 para salvar uma nova senha ou 2 para visualizar uma senha salva')
+
+match action:
+    case '1':
+        if len(Password.get()) == 0:
+            key, path = FernetHasher.create_key(archive=True)
+            print('Chave criada com sucesso. Guarde-a com cuidado!')
+            print(f'Chave: {key.decode("utf-8")}')
+            if path:
+                print('Chave salva no arquivo, lembre-se de remover o arquivo após o transferir de local')
+                print(f'Caminho: {path}')
+
+        else:
+            key = input('Digite sua chave usada para criptografia, use sempre a mesma chave: ')
+
+        domain = input('Domínio: ')
+        password = input('Senha: ')
+        fernet_user = FernetHasher(key)
+        p1 = Password(domain=domain, password=fernet_user.encrypt(password).decode('utf-8'))
+        p1.save()   
+        
+    case '2':
+        ...
